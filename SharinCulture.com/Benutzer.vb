7ac61a950aff558930ee33f0496b28c62fb714b1
maxseidlitz <61022094+maxseidlitz@users.noreply.github.com>
2021-05-23 16:35:52 +0200

    Entwurfsklassendiagramm eingefügt (v1) + Weitere Anpassung der Dtaenbank

---
+++ SharinCulture.com/Benutzer.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Web
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.ComponentModel
+
+Public Class Benutzer
+    Public Property strBenutzername As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property strName As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property strVorname As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property strEmail As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property strTelefonnummer As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property strPasswort As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property datGeburtsdatum As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property bolIstKoordinator As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property bolIstEigentümer As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property intNachbarschaft As Integer
+        Get
+            Return Nothing
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+
+    Public Property Nachbarschaft As NachbarschaftenListe
+        Get
+            Return Nothing
+        End Get
+        Set(value As NachbarschaftenListe)
+        End Set
+    End Property
+
+    Public Property Gegenstand As Gegenstand
+        Get
+            Return Nothing
+        End Get
+        Set(value As Gegenstand)
+        End Set
+    End Property
+End Class
