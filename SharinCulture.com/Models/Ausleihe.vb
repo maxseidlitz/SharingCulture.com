76e8e95a9ac6568a485c374fca1963a2fb2eb6ed
maxseidlitz <61022094+maxseidlitz@users.noreply.github.com>
2021-08-09 10:26:21 +0200

    Konstruktoren für Ausleihe erstellt

--- SharinCulture.com/Models/Ausleihe.vb
+++ SharinCulture.com/Models/Ausleihe.vb
@@ -3,36 +3,57 @@ Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
 
-'Test
 
 Public Class Ausleihe
+
+    Private mintID As Integer
+    Private mdatAusleiheStart As Date
+    Private mdatAusleiheEnde As Date
+    Private mGegenstand As Gegenstand
+
+    'Konstruktor parameterlos
+    Public Sub New()
+        mintID = -1
+        mdatAusleiheStart = Date.Today
+        mdatAusleiheEnde = Date.Today
+        mGegenstand = Nothing
+    End Sub
+
+    'Konstruktor mit Parametern
+    Public Sub New(pintID As Integer, pdatAusleiheStart As Date, pdatAusleiheEnde As Date, pGegenstand As Gegenstand)
+        mintID = pintID
+        mdatAusleiheStart = pdatAusleiheStart
+        mdatAusleiheEnde = pdatAusleiheEnde
+        mGegenstand = pGegenstand
+    End Sub
+
     Public Property intID As Integer
         Get
-            Return Nothing
+            Return mintID
         End Get
         Set(value As Integer)
         End Set
     End Property
 
-    Public Property datAusleiheStart As Integer
+    Public Property datAusleiheStart As Date
         Get
-            Return Nothing
+            Return mdatAusleiheStart
         End Get
-        Set(value As Integer)
+        Set(value As Date)
         End Set
     End Property
 
-    Public Property datAusleiheEnde As Integer
+    Public Property datAusleiheEnde As Date
         Get
-            Return Nothing
+            Return mdatAusleiheEnde
         End Get
-        Set(value As Integer)
+        Set(value As Date)
         End Set
     End Property
 
     Public Property Gegenstand As Gegenstand
         Get
-            Return Nothing
+            Return mGegenstand
         End Get
         Set(value As Gegenstand)
         End Set
