7207413e39b3c189f764f7dc8b6a28ea8718ea5f
maxseidlitz <61022094+maxseidlitz@users.noreply.github.com>
2021-08-09 11:19:17 +0200

    Erstellen von Konstruktoren, Attributen und allg. Prozeduren (hinzufuegen, leeren) für die List-Klassen [GegenstaendeListe, NachbarschaftenListe, BenutzerListe, KategorienListe]

---
+++ SharinCulture.com/BenutzerListe.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Web
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.ComponentModel
+
+Public Class BenutzerListe
+    Private mlstBenutzer As List(Of Benutzer)
+
+    'Konstruktor parameterlos
+    Public Sub New()
+        mlstBenutzer = New List(Of Benutzer)
+    End Sub
+    'Konstruktor mit Parametern
+    Public Sub New(plstBenutzer As List(Of Benutzer))
+        mlstBenutzer = plstBenutzer
+    End Sub
+
+    Public Property Benutzer As List(Of Benutzer)
+        Get
+            Return mlstBenutzer
+        End Get
+        Set(value As List(Of Benutzer))
+            mlstBenutzer = value
+        End Set
+    End Property
+
+    Public Sub hinzufuegen(pBen As Benutzer)
+        mlstBenutzer.Add(pBen)
+    End Sub
+
+    Public Sub leeren()
+        mlstBenutzer.Clear()
+    End Sub
+
+
+End Class
--- SharinCulture.com/Models/NachbarschaftenListe.vb
+++ SharinCulture.com/Models/NachbarschaftenListe.vb
@@ -5,15 +5,36 @@ Imports System.ComponentModel
 
 Public Class NachbarschaftenListe
 
-    Public Property Nachbarschaft As Nachbarschaft
+    Private mlstNachbarschaften As List(Of Nachbarschaft)
+
+    'Konstruktor parameterlos
+    Public Sub New()
+        mlstNachbarschaften = New List(Of Nachbarschaft)
+    End Sub
+
+    'Konstruktor mit Parametern
+    Public Sub New(plstNachbarschaften As List(Of Nachbarschaft))
+        mlstNachbarschaften = plstNachbarschaften
+    End Sub
+
+    Public Property Nachbarschaft As List(Of Nachbarschaft)
         Get
-            Return Nothing
+            Return mlstNachbarschaften
         End Get
-        Set(value As Nachbarschaft)
+        Set(value As List(Of Nachbarschaft))
         End Set
     End Property
 
     Public Sub LadenAlleNachbarschaften()
 
     End Sub
+
+    Public Sub hinzufuegen(pNach As Nachbarschaft)
+        mlstNachbarschaften.Add(pNach)
+    End Sub
+
+    Public Sub leeren()
+        mlstNachbarschaften.Clear()
+    End Sub
+
 End Class
